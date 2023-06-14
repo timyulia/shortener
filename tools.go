c1ab80023ff4b3e3f07cd6193d01aea28da3c68b
timyulia <timoshenkojulie@icloud.com>
2023-06-14 18:53:18 +0300

    remove unused imports

--- tools.go
+++ tools.go
@@ -5,8 +5,4 @@ package tools
 
 import (
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
